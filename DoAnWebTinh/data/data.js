37c546221d5520440c45759df669de894fbbf733
nguyenthequang0802 <104448411+nguyenthequang0802@users.noreply.github.com>
2023-08-08 23:54:46 +0700

    Update work on 8/8/2023

---
+++ DoAnWebTinh/data/data.js
@@ -0,0 +1,387 @@
+products = [
+    {
+        id: "01",
+        name: "Adidas UltraBoost",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/adidas-ultraboost-20-metallic-gold-fv83511-king-shoes-sneaker-real-hcm-1624253902.jpeg",
+        price: 2300000,
+        discount: 10,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "02",
+        name: "Adidas Ultra 4D",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/hq0949-giay-adidas-ultra-4d-chinh-hang-gia-tot-den-king-shoes-1.jpg",
+        price: 4800000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "03",
+        name: "Adidas Stan Smith",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/gz2202-giay-adidas-stan-smith-andre-saraiva-chinh-hang-gia-tot-den-king-shoes-1.jpg",
+        price: 2200000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "04",
+        name: "Adidas NMD R1",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/adidas-nmd-r1-all-white-h019031-king-shoes-sneaker-real-hcm-1624253160.jpeg",
+        price: 2500000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "05",
+        name: "Adidas NMD R2",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/SNEAKER-CQ2404-NMD-R2-adidas-kingshoes.vn-tphcm-tanbinh-20-logo.jpg",
+        price: 2500000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "06",
+        name: "Adidas Samba Slassic",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/772109-giay-adidas-samba-classic-chinh-hang-gia-tot-den-king-shoes1.jpg",
+        price: 2800000,
+        discount: 11,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "07",
+        name: "Adidas Solar HU NMD",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/adidas-solar-hu-nmd-ee7579-king-shoes-sneaker-real-hcm-1624252655.jpeg",
+        price: 5800000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "08",
+        name: "Adidas AlphaBoost",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/ef1183-gia%CC%80y-adidas-alphaboost-chi%CC%81nh-ha%CC%83ng-de%CC%82%CC%81n-king-shoes-1-1653289514.jpg",
+        price: 2900000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "09",
+        name: "Adidas NMD V3",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/gx3378-giay-nmd-v3-chinh-hang-gia-tot-den-king-shoes-1.jpg",
+        price: 3800000,
+        discount: 24,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "10",
+        name: "Adidas Forum Low Green",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/gia%CC%80y-adidas-forum-low-green-gy8556-chi%CC%81nh-ha%CC%83ng-de%CC%82%CC%81n-king-shoes-1-1648013432.jpg",
+        price: 2900000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "11",
+        name: "Adidas Sense Boost Go",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/eg0959-giay-adidas-sense-boost-go-chinh-hang-gia-tot-den-king-shoes-1.jpg",
+        price: 2800000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "12",
+        name: "Adidas Grand Court",
+        brand: "Adidas",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/ee7888-giay-adidas-grand-court-chinh-hang-gia-tot-den-king-shoes-1.jpg",
+        price: 2300000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+
+    {
+        id: "13",
+        name: "Nike Air Force 1 Serena",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/dm5036-100-giay-nike-air-force-1-serena-summit-white-chinh-hang-gia-tot-den-king-shoes-6.jpg",
+        price: 3600000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "14",
+        name: "Nike Pegasus 40",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/dv7480-100-giay-chay-bo-nike-pegasus-40-wide-chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 3900000,
+        discount: 8,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "15",
+        name: "Nike Air Force 1",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/SNEAKER-315122-111-AIR-FORCE-1-07-NIKE-KINGSHOES.VN-TPHCM-TANBINH-17-logo-1551924204-.jpg",
+        price: 3300000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "16",
+        name: "Nike Air Max 97 All Star",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/nike-air-max-97-all-star-king-shoes-sneaker-real-hcm-1624258705.jpeg",
+        price: 4600000,
+        discount: 50,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "16",
+        name: "Nike Air Max 97 All Star",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/nike-air-max-97-all-star-king-shoes-sneaker-real-hcm-1624258705.jpeg",
+        price: 4600000,
+        discount: 50,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "17",
+        name: "Nike KillShot 2 Midnight Navy",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/432997-107-giay-nike-killshot-2-leather-midnight-navy-chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 2900000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "17",
+        name: "Nike Air Zoom Rival Fly 3",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/gia%CC%80y-nike-air-zoom-rival-fly-3-road-racing-dj5427-100-king-shoes-sneaker-real-hcm-12-1638339891.jpeg",
+        price: 2900000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "18",
+        name: "Nike Air Max 1 PRM",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/fd5088-200-giay-nike-air-max-1-prm-chinh-hang-gia-tot-den-king-shoes-13.jpeg",
+        price: 4200000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "19",
+        name: "Nike Jordan 1 Low Craft",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/dn1635-100-giay-nike-air-jordan-1-low-inside-out-chinh-hang-gia-tot-den-king-shoes-15.jpeg",
+        price: 4800000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "20",
+        name: "Nike Jordan 4 Retro SE",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/dv3742-021-giay-nike-air-jordan-4-retro-se-craft-photon-dust-chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 9500000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "20",
+        name: "Nike Jordan 1 Low Diamond",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/gia%CC%80y-nike-air-jordan-1-low-black-and-white-dh6931-001-chi%CC%81nh-ha%CC%83ng-de%CC%82%CC%81n-king-shoes-8-1651910376.jpeg",
+        price: 6200000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "21",
+        name: "Nike Jordan 1 HI OG",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/do9369-101-giay-nike-air-jordan-1-starfish-chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 4800000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "22",
+        name: "Nike Dunk Low Black Paisley",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/dh4401-100-gia%CC%80y-nike-dunk-low-black-paisley-chi%CC%81nh-ha%CC%83ng-de%CC%82%CC%81n-king-shoes-12-1654932194.jpeg",
+        price: 7200000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "23",
+        name: "Nike Air Jordan 5 Low PSG",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/dx6325-204-giay-nike-air-jordan-5-low-psg-chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 6800000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "24",
+        name: "Nike Dunk Low Retro",
+        brand: "Nike",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/dd1391-600-gia%CC%80y-nike-dunk-low-retro-chi%CC%81nh-ha%CC%83ng-de%CC%82%CC%81n-king-shoes-4-1660122102.jpg",
+        price: 6800000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "25",
+        name: "Yeezy Boost 350 V2",
+        brand: "Yeezy",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/gia%CC%80y-adidas-yeezy-boost-350-v2-beluga-reflective-gw1229-king-shoes-sneaker-real-hcm-11-1639905369.jpeg",
+        price: 6000000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "26",
+        name: "Yeezy Boost 380",
+        brand: "Yeezy",
+        category: "Giay",
+        image: "https://kingshoes.vn/data/upload/media/gz8668-giay-adidas-yeezy-boost-380--chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 8000000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "27",
+        name: "Dép Adidas Adilette TND",
+        brand: "Adidas",
+        category: "Dep",
+        image: "https://kingshoes.vn/data/upload/media/gz8668-giay-adidas-yeezy-boost-380--chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 950000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "28",
+        name: "Dép Yeezy Slide Flax",
+        brand: "Yeezy",
+        category: "Dep",
+        image: "https://kingshoes.vn/data/upload/media/fz5896-dep-yeezy-slide-flax-chinh-hang-gia-tot-den-king-shoes-2.jpg",
+        price: 1200000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "29",
+        name: "Dép Jordan Hex Mule SP",
+        brand: "Nike",
+        category: "Dep",
+        image: "https://kingshoes.vn/data/upload/media/fd2652-002-dep-jordan-hex-mule-sp-chinh-hang-gia-tot-den-king-shoes-12.jpeg",
+        price: 1400000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "30",
+        name: "Dép Jordan Break Slide Red",
+        brand: "Nike",
+        category: "Dep",
+        image: "https://kingshoes.vn/data/upload/media/ar6374-602-dep-jordan-break-slides-chinh-hang-gia-tot-den-king-shoes-3.png",
+        price: 1250000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "31",
+        name: "Dép Nike Asuna Slide Phonton Dust",
+        brand: "Nike",
+        category: "Dep",
+        image: "https://kingshoes.vn/data/upload/media/de%CC%81p-nike-asuna-slide-photon-dust-cw9707-001-king-shoes-sneaker-real-hcm-3-1635830143.jpg",
+        price: 1300000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+
+    {
+        id: "32",
+        name: "Dép Puma",
+        brand: "Puma",
+        category: "Dep",
+        image: "https://kingshoes.vn/data/upload/media/de%CC%81p-puma-slide-rainbow-383408-01-king-shoes-sneaker-real-hcm-1-1636091114.jpeg",
+        price: 500000,
+        discount: 0,
+        size: ["38", "38.5", "39", "39.5", "40", "40.5", "41", "42"]
+    },
+]
