db476e835b68d59355006f52791e0a18f96afcf5
nguyenthequang0802 <104448411+nguyenthequang0802@users.noreply.github.com>
2023-10-11 01:09:33 +0700

    update slider carousel homePage

--- DoAnWebTinh/js/index.js
+++ DoAnWebTinh/js/index.js
@@ -57,3 +57,24 @@ $('.product-item-1').on('click', '.ps-shoe', function(){
     window.location.href = `../pages/detailProduct.html?id=${productId}`;
 })
 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 1500,
+    dotsSpeed: 1500,
+    laxyload: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
